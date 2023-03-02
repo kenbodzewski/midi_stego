b25bd1620a892975734e3b3b467c7692e23c499d
Ken <bodzewski.k@northeastern.edu>
2023-03-02 13:17:35 -0500

    added additional conversion

--- src/text_conversion.py
+++ src/text_conversion.py
@@ -8,3 +8,17 @@ def message_to_hex(message):
 # converts a hex string into an ascii string
 def hex_to_message(hex_message):
     return bytearray().fromhex(hex_message).decode()
+
+# converst a hex number into a base 10 integer
+def hex_to_int(hex):
+    return int(hex, 16)
+
+# convert from hex
+
+# TODO:
+# convert from int to hex
+# look into maximum size for velocity as this will determine 
+
+# print(chr(97))
+
+# print(ord('a'))
